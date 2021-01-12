f9df6bd0168821c8439c8a64680fd50057bc2aa4
sihyunlove <sihyunlove@zohomail.com>
2021-01-12 15:38:56 +0900

    start sihyunlove wiki

---
+++ customize.js
@@ -0,0 +1,58 @@
+/* Customization Guide available on:
+  https://gatsby-blog-mdx.now.sh/2020/05/4-customize/
+  ======================================== */
+const config = {
+  /* Site
+  ========================================= */
+  title: `SihyunLove's Development Reference`,          // Displayed in header
+  description: `SihyunLove's Development Reference`,     // Site description
+  maxWidth: "768px",                  // Max width of website
+  enableAbout: true,                  // Enables about page
+  siteUrl: `https://sihyunlove.netlify.app/`, // For sitemap & RSS feed
+  useScrollIndicator: true,           // Use scroll indicator on top of screen in posts
+
+  /* Profile
+  ========================================= */
+  author: `권혁진`,                             // Name shows on profile
+  profileDescription: "C/C++, MFC가 주력이며 docker, vagrant, node.js, react에 관심이 많습니다.",   // Shows under author name
+  profileImageName: `profile.jpeg`,                  // Place profile.jpg/.jpeg/.png in _assets folder
+  location: "",                                      // Location under profileDescription. "" --> disabled
+  footerLink: "",                                    // Link to page when you click footer name
+
+  /* Social Media Links
+      accountName & emailAddress: leave blank ("") to disable
+      showHeaderIcon: shows social media icon in header. When true, must have account name set
+  ========================================= */
+  socialMediaLinks: {
+    email:     { emailAddress: "sihyunlove@zohomail.com", showHeaderIcon: true },
+    github:    { accountName: "sihyunlove", showHeaderIcon: true },
+    facebook:  { accountName: "", showHeaderIcon: true },
+    instagram: { accountName: "", showHeaderIcon: true },
+    twitter:   { accountName: "", showHeaderIcon: true },
+    linkedIn:  { accountName: "", showHeaderIcon: true },   // Use URL after "linkedin.com/"
+    medium:    { accountName: "", showHeaderIcon: true },   // Use URL after "medium.com/@"
+  },
+
+  /* Social Media Share Buttons--available below every post
+  ========================================= */
+  shareButtons: { email: true, facebook: false, twitter: false, reddit: false, linkedIn: false },
+
+  /* Comments
+  ========================================= */
+  comments: {
+    facebook: { enabled: false, appId: "" },      // appId: optional, but used to moderate fb comments
+    disqus: { enabled: false, shortName: "" },   // Check your shortName on https://disqus.com
+    utterances: { enabled: false, repoUrl: "" }, // i.e. 'ellismin/gatsby-blog-mdx'
+  },
+
+  /* More Custom Settings
+  ========================================= */
+  defaultTheme: "dark",                      // Options: dark / light -- applied to first visitors
+  showTimeToRead: false,                      // Shows time to read for each post in main page
+  breakCodeLines: false,                      // Breaks long code lines instead of having horizontal scroll
+  faviconSrc: `_assets/icon-code.png`,        // Favicon
+  gaTrackingId: ``,                           // Your google analytics tracking id--i.e. UA-*****
+  googleAdSenseId: ``,                        // Your google AdSense client id--i.e. ca-pub-****
+}
+
+module.exports = config
